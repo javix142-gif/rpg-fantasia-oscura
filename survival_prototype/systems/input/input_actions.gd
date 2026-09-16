1e722e9b041f784f545048031e12fe6eb4822136
javix142-gif <javix142@gmail.com>
2026-09-15 22:10:48 -0300

    Centralize keyboard InputMap bindings

--- survival_prototype/systems/input/input_actions.gd
+++ survival_prototype/systems/input/input_actions.gd
@@ -1,6 +1,35 @@
 class_name CSInputActions
 extends RefCounted
 
+const KEY_BINDINGS := {
+	"move_left": [KEY_A, KEY_LEFT],
+	"move_right": [KEY_D, KEY_RIGHT],
+	"move_up": [KEY_W, KEY_UP],
+	"move_down": [KEY_S, KEY_DOWN],
+	"sprint": [KEY_SHIFT],
+	"attack": [KEY_SPACE],
+	"interact": [KEY_E],
+	"inventory": [KEY_I, KEY_TAB],
+	"craft_menu": [KEY_C],
+	"build_menu": [KEY_B],
+	"map": [KEY_M],
+	"eat": [KEY_F],
+	"bandage": [KEY_H],
+	"cancel": [KEY_ESCAPE],
+}
+
+static func ensure_actions() -> void:
+	for action_variant in KEY_BINDINGS.keys():
+		var action := StringName(action_variant)
+		if not InputMap.has_action(action):
+			InputMap.add_action(action)
+		if not InputMap.action_get_events(action).is_empty():
+			continue
+		for code_variant in KEY_BINDINGS[action_variant]:
+			var event := InputEventKey.new()
+			event.keycode = int(code_variant)
+			InputMap.action_add_event(action, event)
+
 static func movement_vector() -> Vector2:
 	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
